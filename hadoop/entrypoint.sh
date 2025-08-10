2a1c1ae9d6d883ffe2ff7daac72fc1bab6ee46c6
tcnam <56421023+tcnam@users.noreply.github.com>
2025-08-10 16:48:33 +0700

    checkpoint 1.2: add edge node, spark

--- hadoop/entrypoint.sh
+++ hadoop/entrypoint.sh
@@ -16,16 +16,17 @@ elif [ "$NODE_TYPE" == "secondarynamenode" ]; then
 
 elif [ "$NODE_TYPE" == "resourcemanager" ]; then
     su - yarn -c "yarn --daemon start resourcemanager"
-    su - spark -c "start-master.sh"
+    # su - spark -c "start-master.sh"
 
 elif [ "$NODE_TYPE" == "worker" ]; then 
     su - hdfs -c "hdfs --daemon start datanode"
     su - yarn -c "yarn --daemon start nodemanager"
-    su - spark -c "start-worker.sh"
+    # su - spark -c "start-worker.sh"
 
 elif [ "$NODE_TYPE" == "historyserver" ]; then
     su - yarn -c "mapred --daemon start historyserver"
-
+elif [ "$NODE_TYPE" == "edgenode" ]; then
+    su - root -c "tail -f /dev/null"
 else
     echo "Unknown NODE_TYPE: $NODE_TYPE"
     exit 1
--- hadoop/start-hdfs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -d "/opt/hadoop/data/nameNode/current" ]; then
-    echo "Formatting NameNode..."
-    hdfs namenode -format
-fi
-hdfs namenode
-# source /opt/hadoop/sbin/start_all.sh
