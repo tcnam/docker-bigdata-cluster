53e9d76d16fc5de4e80c537cc339757df3da2796
Nam Tran <tcnam2000@gmail.com>
2025-09-20 20:50:41 +0700

    update entrypoint.sh script: add user folder to hdfs

--- hadoop/entrypoint.sh
+++ hadoop/entrypoint.sh
@@ -24,8 +24,18 @@ elif [ "$NODE_TYPE" == "worker" ]; then
 elif [ "$NODE_TYPE" == "historyserver" ]; then
     su - yarn -c "mapred --daemon start historyserver"
     su - root -c "start-history-server.sh"
+
     su - hdfs -c "hdfs dfs -mkdir -p /spark/logs"
     su - hdfs -c "hdfs dfs -chown -R spark:hadoop /spark/logs"
+
+    su - hdfs -c "hdfs dfs -mkdir -p /user/spark"
+    su - hdfs -c "hdfs dfs -chown spark:supergroup /user/spark"
+
+    su - hdfs -c "hdfs dfs -mkdir -p /user/yarn"
+    su - hdfs -c "hdfs dfs -chown yarn:supergroup /user/yarn"
+
+    su - hdfs -c "hdfs dfs -mkdir -p /user/mapred"
+    su - hdfs -c "hdfs dfs -chown mapred:supergroup /user/mapred"
 elif [ "$NODE_TYPE" == "edgenode" ]; then
     su - hdfs -c "hdfs dfs -mkdir -p /spark/logs"
     tail -f /dev/null
